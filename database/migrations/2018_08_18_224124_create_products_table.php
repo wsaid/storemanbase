50fa4bcd8a310b69df2c21f17ff02625fb8ae8af
Walid Said <wsaidamin@gmail.com>
2018-08-19 05:42:16 +0200

    add product model and migration

---
+++ database/migrations/2018_08_18_224124_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('quantperunit', 20);
+            $table->decimal('unitprice', 10, 4);
+            $table->unsignedSmallInteger('unitinstock');
+            $table->binary('discontinued');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
